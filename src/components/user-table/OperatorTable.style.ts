85450fa153ba2fd9f13bbbbfd2c577240cb62166
Nikita <nikita.shkuratt@gmail.com>
2024-09-06 00:12:02 +0300

    added operators table component

---
+++ src/components/user-table/OperatorTable.style.ts
@@ -0,0 +1,11 @@
+export const styles = {
+  avatarTableCell: {
+    display: 'flex',
+    gap: '8px',
+    alignItems: 'center'
+  },
+  tableCellIndex: {
+    color: 'rgba(41, 36, 36, 1)',
+    fontWeight: '500'
+  }
+}
---
+++ src/components/user-table/OperatorTable.tsx
@@ -0,0 +1,177 @@
+import React, { useEffect, useState, useMemo, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Avatar,
+  Checkbox,
+  TextField,
+  Box,
+  TableSortLabel,
+  Typography,
+  Divider
+} from '@mui/material'
+import { getOperatorListAction } from '~/store/slices/users.slice'
+import { RootState } from '~/store'
+import { OperatorType, SortConfig } from '~/types'
+
+import { styles } from './OperatorTable.style'
+
+const OperatorTable: React.FC = () => {
+  const dispatch = useDispatch()
+  const { data, isLoading, errors } = useSelector(
+    (state: RootState) => state.users.list
+  )
+
+  const [searchTerm, setSearchTerm] = useState<string>('')
+  const [sortConfig, setSortConfig] = useState<SortConfig>({
+    key: 'name',
+    direction: 'asc'
+  })
+
+  useEffect(() => {
+    dispatch(getOperatorListAction())
+  }, [dispatch])
+
+  const handleSearch = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchTerm(event.target.value)
+    },
+    []
+  )
+
+  const handleSort = useCallback((key: keyof OperatorType) => {
+    setSortConfig((prevConfig) => ({
+      key,
+      direction:
+        prevConfig.key === key && prevConfig.direction === 'asc'
+          ? 'desc'
+          : 'asc'
+    }))
+  }, [])
+
+  const sortedData = useMemo(() => {
+    if (!data) return null
+
+    const { key, direction } = sortConfig
+    return [...data].sort((a, b) => {
+      if (key === 'isWorking') {
+        return direction === 'asc'
+          ? Number(a[key]) - Number(b[key])
+          : Number(b[key]) - Number(a[key])
+      } else if (key === 'createdAt') {
+        return direction === 'asc'
+          ? new Date(a[key]).getTime() - new Date(b[key]).getTime()
+          : new Date(b[key]).getTime() - new Date(a[key]).getTime()
+      } else {
+        const aValue = String(a[key])
+        const bValue = String(b[key])
+        if (aValue < bValue) return direction === 'asc' ? -1 : 1
+        if (aValue > bValue) return direction === 'asc' ? 1 : -1
+        return 0
+      }
+    })
+  }, [data, sortConfig])
+
+  const filteredData = useMemo(() => {
+    if (!sortedData) return null
+
+    return sortedData.filter((operator) =>
+      operator.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  }, [sortedData, searchTerm])
+
+  if (isLoading) return <div>Loading...</div>
+  if (errors) return <div>Error: {JSON.stringify(errors)}</div>
+
+  return (
+    <TableContainer component={Paper}>
+      <Box p={2}>
+        <TextField
+          label="Ім'я користувача"
+          variant='outlined'
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </Box>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>#</TableCell>
+            <TableCell>
+              <TableSortLabel
+                active={sortConfig.key === 'name'}
+                direction={sortConfig.direction}
+                onClick={() => handleSort('name')}
+              >
+                <Typography variant='tableHeader'>Користувач</Typography>
+              </TableSortLabel>
+            </TableCell>
+            <TableCell>
+              <TableSortLabel
+                active={sortConfig.key === 'isWorking'}
+                direction={sortConfig.direction}
+                onClick={() => handleSort('isWorking')}
+              >
+                <Typography variant='tableHeader'>Працює</Typography>
+              </TableSortLabel>
+            </TableCell>
+            <TableCell>
+              <TableSortLabel
+                active={sortConfig.key === 'createdAt'}
+                direction={sortConfig.direction}
+                onClick={() => handleSort('createdAt')}
+              >
+                <Typography variant='tableHeader'>
+                  Дата / Час створення
+                </Typography>
+              </TableSortLabel>
+            </TableCell>
+            <TableCell>
+              <Typography variant='tableHeader'>SMTP</Typography>
+            </TableCell>
+            {/* <TableCell>
+              <Typography variant='tableHeader'>JBOD</Typography>
+            </TableCell> */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredData?.map((operator, index) => (
+            <TableRow key={operator.id}>
+              <TableCell>
+                <Typography variant='body2' sx={styles.tableCellIndex}>
+                  {index + 1}
+                </Typography>
+              </TableCell>
+              <TableCell sx={styles.avatarTableCell}>
+                <Avatar src={operator.avatar} />
+                <Typography variant='body2'>{operator.name}</Typography>
+              </TableCell>
+              <TableCell>
+                <Checkbox checked={operator.isWorking} />
+              </TableCell>
+              <TableCell>
+                <Typography variant='body2'>
+                  {new Date(operator.createdAt).toLocaleString()}
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant='body2'>{operator.SMTP}</Typography>
+              </TableCell>
+              {/* <TableCell>
+                <Typography variant='body2'>{operator.JBOD}</Typography>
+              </TableCell> */}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default OperatorTable
